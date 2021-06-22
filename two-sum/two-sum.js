03c4c48b1686e459597bcf5413b99a834ea411ea
Sunil Gadgil <sunilrgadgil@gmail.com>
2021-06-22 12:04:43 -0500

    Time: 64 ms (99.81%), Space: 40.7 MB (15.41%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,18 +3,19 @@
  * @param {number} target
  * @return {number[]}
  Strategy: 
- Iterate through nums (i)
- Iterate through nums starting at i (j)
- If i + j adds up to target,
-    return i + j
+
+    
+
  */
+
 var twoSum = function(nums, target) {
+    let map = {};
+    
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        let complement = target - nums[i];
+        if (map[complement] !== undefined) {
+            return [i, map[complement]]; // [1, 0]
         }
+        map[nums[i]] = i;
     }
-    return null;
 };
