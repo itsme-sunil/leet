e1f0ebb76c94e60b4f7be673a32c5c8577d5475d
Sunil Gadgil <sunilrgadgil@gmail.com>
2021-06-30 11:37:26 -0500

    Time: 224 ms (5.01%), Space: 57.6 MB (5.10%) - LeetHub

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -1,15 +1,4 @@
-/**
- * @param {string} s
- * @return {boolean}
- Strategy: recursive
- ** edge case: if length of string is an odd number, return false
- 
- ** recursive case: iterate through string. If we find matching consecutive parens, reassign string to a sliced version of the string without the matching parens.
- return isValid on sliced string.
- 
- ** base case: if string has length, return false. otherwise, return true.
- 
- */
+
 var isValid = function(s) {
     if (s.length % 2 !== 0) {
         return false;
@@ -23,11 +12,7 @@ var isValid = function(s) {
         }
     }
     
-    if (s.length > 0) {
-        return false;
-    } else {
-        return true;
-    }
+    return s.length < 1;
 };
 
 const matchingParens = {
