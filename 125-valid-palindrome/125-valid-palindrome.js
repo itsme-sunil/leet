cccdeec83eda5b939c3b5f2c1c92bf0e7c146430
Sunil Gadgil <sunil.gadgil@dealerware.com>
2022-05-09 09:53:30 -0500

    Time: 103 ms (44.77%), Space: 45.2 MB (57.46%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    // filter out non alphanumeric characters with regex
     s = s.match(/[A-Z0-9]+/gi)?.join('').toLowerCase();
     if (!s?.length) return true;
         
-    // same loop, checking backwards and forwards for palindrome chars
-    for (let i = 0; i < s.length; i++) {
-        const j = s.length - 1 - i;
-        // return true if two indices are the same or if i > j
-        if (i >= j) return true;
-        // return false if we find a mismatch
+    let i = 0;
+    let j = s.length - 1;
+    
+    while (i < j) {
         if (s[i] !== s[j]) return false;
+        i++;
+        j--;
     }
+    return true;
 };
