90766ebbe85b7bd60a70e1797f9c0bb5d3cd7051
Sunil Gadgil <sunil.gadgil@dealerware.com>
2022-05-09 11:56:58 -0500

    Time: 97 ms (52.35%), Space: 44.9 MB (61.34%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,13 +6,19 @@ var isPalindrome = function(s) {
     s = s.match(/[A-Z0-9]+/gi)?.join('').toLowerCase();
     if (!s?.length) return true;
         
-    let i = 0;
-    let j = s.length - 1;
-    
-    while (i < j) {
+    for (let i = 0; i < s.length; i++) {
+        const j = s.length - 1 - i;
+        if (i >= j) return true;
         if (s[i] !== s[j]) return false;
-        i++;
-        j--;
     }
-    return true;
+    
+    // let i = 0;
+    // let j = s.length - 1;
+    
+    // while (i < j) {
+    //     if (s[i] !== s[j]) return false;
+    //     i++;
+    //     j--;
+    // }
+    // return true;
 };
