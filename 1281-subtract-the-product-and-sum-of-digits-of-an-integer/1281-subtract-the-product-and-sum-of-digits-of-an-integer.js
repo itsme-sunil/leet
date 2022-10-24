2a789ac566001fb67c3733975b648e7624411849
Sunil Gadgil <sunil.gadgil@dealerware.com>
2022-10-24 09:06:16 -0500

    Time: 98 ms (44.94%), Space: 42.3 MB (29.23%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -5,8 +5,16 @@
 var subtractProductAndSum = function(n) {
     n = n.toString().split('').map(n => Number(n));
     
-    const product = n.reduce((product, factor) => product * factor);
-    const sum = n.reduce((sum, addend) => sum + addend);
+    // const product = n.reduce((product, factor) => product * factor);
+    // const sum = n.reduce((sum, addend) => sum + addend);
+    
+    let product = 1;
+    let sum = 0;
+    
+    n.forEach(n => {
+        product *= n;
+        sum += n;
+    });
     
     return product - sum;
 };
